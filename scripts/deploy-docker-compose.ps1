0401eb9f2cc77bc3cc5a1fb2ece6b2883daf0273
Gabriele Tronchin <gabriele.tronchin@reti.it>
2024-04-19 21:59:08 +0200

    adjust repo

--- scripts/deploy-docker-compose.ps1
+++ scripts/deploy-docker-compose.ps1
@@ -1,1 +1,2 @@
-docker compose --env-file ../config/.env -f  ../docker-compose/docker-compose-redis.yml up -d
+# just a sample, modify this script accorting to your needs
+docker compose --env-file ../config/.env -f ../docker-compose/docker-compose-redis.yml up -d
